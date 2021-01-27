b8f1c179acaf66e454b29dba2336ac965915f8a6
Devon Findlay <devonson4@gmail.com>
2021-01-27 12:54:19 +0000

    Testing framework added
    Use ./gradlew tests:test to run tests

---
+++ tests/src/com.randomlygenerated.tests/Example.java
@@ -0,0 +1,20 @@
+package com.randomlygenerated.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Example {
+
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(false);
+    }
+
+}
